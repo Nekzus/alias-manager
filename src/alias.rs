83e13c98042018f6b260f555a7a362a8d9711a18
Nekzus <85507589+Nekzus@users.noreply.github.com>
2024-07-06 21:03:29 -0300

    dir aliases

--- src/alias.rs
+++ src/alias.rs
@@ -2,7 +2,7 @@ use crate::file_ops::{self, Alias};
 use console::Style;
 use dialoguer::{theme::ColorfulTheme, Input, Select};
 
-pub fn add_alias(file_path: &str) {
+pub fn add_alias(file_path: Option<&str>) {
     let alias_name: String = Input::with_theme(&ColorfulTheme::default())
         .with_prompt("Enter alias name")
         .interact_text()
@@ -24,103 +24,119 @@ pub fn add_alias(file_path: &str) {
         description: alias_description.clone(),
     };
 
-    if let Err(err) = file_ops::append_to_file(file_path, &alias) {
-        eprintln!("Error adding alias: {}", err);
-        return;
-    }
+    if let Some(file_path_str) = file_path {
+        if let Err(err) = file_ops::append_to_file(file_path_str, &alias) {
+            eprintln!("Error adding alias: {}", err);
+            return;
+        }
 
-    let style = Style::new().green();
-    println!("{}", style.apply_to(format!("Alias added: {}", alias.name)));
-    println!("Command: {}", alias.command);
-    println!("Description: {}", alias.description);
-    println!();
+        let style = Style::new().green();
+        println!("{}", style.apply_to(format!("Alias added: {}", alias.name)));
+        println!("Command: {}", alias.command);
+        println!("Description: {}", alias.description);
+        println!();
+    } else {
+        eprintln!("Error: file_path is None");
+    }
 }
 
-pub fn edit_alias(file_path: &str) {
-    let aliases = file_ops::load_aliases(file_path);
-    let alias_names: Vec<_> = aliases.iter().map(|a| a.name.clone()).collect();
+pub fn edit_alias(file_path: Option<&str>) {
+    if let Some(file_path_str) = file_path {
+        let aliases = file_ops::load_aliases(file_path_str);
+        let alias_names: Vec<_> = aliases.iter().map(|a| a.name.clone()).collect();
 
-    let selection = Select::with_theme(&ColorfulTheme::default())
-        .with_prompt("Select alias to edit")
-        .items(&alias_names)
-        .interact()
-        .unwrap();
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Select alias to edit")
+            .items(&alias_names)
+            .interact()
+            .unwrap();
 
-    let alias = &aliases[selection];
+        let alias = &aliases[selection];
 
-    let new_command: String = Input::with_theme(&ColorfulTheme::default())
-        .with_prompt("Enter new command")
-        .default(alias.command.clone())
-        .interact_text()
-        .unwrap();
+        let new_command: String = Input::with_theme(&ColorfulTheme::default())
+            .with_prompt("Enter new command")
+            .default(alias.command.clone())
+            .interact_text()
+            .unwrap();
 
-    let new_description: String = Input::with_theme(&ColorfulTheme::default())
-        .with_prompt("Enter new description")
-        .default(alias.description.clone())
-        .interact_text()
-        .unwrap();
+        let new_description: String = Input::with_theme(&ColorfulTheme::default())
+            .with_prompt("Enter new description")
+            .default(alias.description.clone())
+            .interact_text()
+            .unwrap();
 
-    let new_alias = Alias {
-        name: alias.name.clone(),
-        command: new_command.clone(),
-        description: new_description.clone(),
-    };
+        let new_alias = Alias {
+            name: alias.name.clone(),
+            command: new_command.clone(),
+            description: new_description.clone(),
+        };
 
-    if let Err(err) = file_ops::update_alias(file_path, &new_alias) {
-        eprintln!("Error updating alias: {}", err);
-        return;
+        if let Err(err) = file_ops::update_alias(file_path_str, &new_alias) {
+            eprintln!("Error updating alias: {}", err);
+            return;
+        }
+
+        let style = Style::new().green();
+        println!(
+            "{}",
+            style.apply_to(format!("Alias updated: {}", new_alias.name))
+        );
+        println!("New command: {}", new_alias.command);
+        println!("New description: {}", new_alias.description);
+        println!();
+    } else {
+        eprintln!("Error: file_path is None");
     }
-
-    let style = Style::new().green();
-    println!(
-        "{}",
-        style.apply_to(format!("Alias updated: {}", new_alias.name))
-    );
-    println!("New command: {}", new_alias.command);
-    println!("New description: {}", new_alias.description);
-    println!();
 }
 
-pub fn remove_alias(file_path: &str) {
-    let aliases = file_ops::load_aliases(file_path);
-    let alias_names: Vec<_> = aliases.iter().map(|a| &a.name).collect();
+pub fn remove_alias(file_path: Option<&str>) {
+    if let Some(file_path_str) = file_path {
+        let aliases = file_ops::load_aliases(file_path_str);
+        let alias_names: Vec<_> = aliases.iter().map(|a| &a.name).collect();
 
-    let selection = Select::with_theme(&ColorfulTheme::default())
-        .with_prompt("Select alias to remove")
-        .items(&alias_names)
-        .interact()
-        .unwrap();
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Select alias to remove")
+            .items(&alias_names)
+            .interact()
+            .unwrap();
 
-    let alias = &aliases[selection];
+        let alias = &aliases[selection];
 
-    if let Err(err) = file_ops::delete_alias(file_path, &alias.name) {
-        eprintln!("Error removing alias: {}", err);
-        return;
+        if let Err(err) = file_ops::delete_alias(file_path_str, &alias.name) {
+            eprintln!("Error removing alias: {}", err);
+            return;
+        }
+
+        let style = Style::new().green();
+        println!(
+            "{}",
+            style.apply_to(format!("Alias removed: {}", alias.name))
+        );
+        println!();
+    } else {
+        eprintln!("Error: file_path is None");
     }
-
-    let style = Style::new().green();
-    println!(
-        "{}",
-        style.apply_to(format!("Alias removed: {}", alias.name))
-    );
-    println!();
 }
 
-pub fn search_alias(file_path: &str) {
-    let aliases = file_ops::load_aliases(file_path);
-    let alias_names: Vec<_> = aliases.iter().map(|a| &a.name).collect();
+pub fn search_alias(file_path: Option<&str>) {
+    if let Some(file_path_str) = file_path {
+        let aliases = file_ops::load_aliases(file_path_str);
+        let alias_names: Vec<_> = aliases.iter().map(|a| &a.name).collect();
 
-    let selection = Select::with_theme(&ColorfulTheme::default())
-        .with_prompt("Select alias to search")
-        .items(&alias_names)
-        .interact()
-        .unwrap();
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Select alias to search")
+            .items(&alias_names)
+            .interact()
+            .unwrap();
 
-    let alias = &aliases[selection];
+        let alias = &aliases[selection];
 
-    let style = Style::new().cyan();
-    println!("{}", style.apply_to(format!("Alias found: {}", alias.name)));
-    println!("Command: {}", alias.command);
-    println!("Description: {}", alias.description);
-    println!();
+        let style = Style::new().cyan();
+        println!("{}", style.apply_to(format!("Alias found: {}", alias.name)));
+        println!("Command: {}", alias.command);
+        println!("Description: {}", alias.description);
+        println!();
+    } else {
+        eprintln!("Error: file_path is None");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ mod file_ops;
 
 use alias::{add_alias, edit_alias, remove_alias, search_alias};
 use dialoguer::{theme::ColorfulTheme, Select};
-use dotenv::dotenv;
-use std::env;
+use dirs::home_dir;
+use std::path::PathBuf;
 
 fn main() {
-    dotenv().ok();
-    let alias_file = env::var("ALIAS_FILE").expect("ALIAS_FILE not set");
-    let alias_file = alias_file.as_str();
+    let alias_file = home_dir().map(|mut path: PathBuf| {
+        path.push(".zsh_aliases");
+        path
+    });
+    let alias_file = alias_file.as_deref().and_then(|path| path.to_str());
     loop {
         let actions = vec!["Add", "Edit", "Remove", "Search", "Exit"];
         let selection = Select::with_theme(&ColorfulTheme::default())
